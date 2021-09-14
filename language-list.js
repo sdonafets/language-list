72daa4f78c17e451c46d6144206b9a0bf69964ff
stefano.desantis <stefano.desantis@gellify.com>
2021-09-14 17:00:21 +0200

    [ELL] Fix support of HI, ML, TL, UR languages

--- language-list.js
+++ language-list.js
@@ -1,6 +1,10 @@
 'use strict'
 const en = require("./lang/en.json");
 const ar = require("./lang/ar.json");
+const hi = require("./lang/hi.json");
+const ml = require("./lang/ml.json");
+const tl = require("./lang/tl.json");
+const ur = require("./lang/ur.json");
 
 let languageNameMap = {};
 let languageCodeMap = {};
@@ -8,7 +12,7 @@ let languageCodeMap = {};
 function populateMaps(data, lang) {
   let nameMap = {};
   let codeMap = {};
-  data.forEach(function(e) {
+  data.forEach(function (e) {
     nameMap[e.language.toLowerCase()] = e.code;
     codeMap[e.code.toLowerCase()] = e.language;
   });
@@ -18,8 +22,13 @@ function populateMaps(data, lang) {
 
 populateMaps(en, "en");
 populateMaps(ar, "ar");
+populateMaps(hi, "hi");
+populateMaps(ml, "ml");
+populateMaps(tl, "tl");
+populateMaps(ur, "ur");
 
 module.exports = LanguageList;
+
 function LanguageList() {
   if (!(this instanceof LanguageList)) {
     return new LanguageList();
@@ -35,30 +44,82 @@ LanguageList.prototype.getLanguageName = function getLanguageNames(code, lang) {
 };
 
 LanguageList.prototype.getLanguageNames = function getLanguageNames(lang) {
-  switch(lang){
+  switch (lang) {
     case "en":
-      return en.map(function(l){return l.language});
+      return en.map(function (l) {
+        return l.language
+      });
     case "ar":
-      return ar.map(function(l){return l.language});
+      return ar.map(function (l) {
+        return l.language
+      });
+    case "hi":
+      return hi.map(function (l) {
+        return l.language
+      });
+    case "ml":
+      return ml.map(function (l) {
+        return l.language
+      });
+    case "tl":
+      return tl.map(function (l) {
+        return l.language
+      });
+    case "ur":
+      return ur.map(function (l) {
+        return l.language
+      });
     default:
       return []
   }
 };
 
 LanguageList.prototype.getLanguageCodes = function getLanguageCodes(lang) {
-  switch(lang){
+  switch (lang) {
     case "en":
-      return en.map(function(l){return l.code});
+      return en.map(function (l) {
+        return l.code
+      });
     case "ar":
-      return ar.map(function(l){return l.code});
+      return ar.map(function (l) {
+        return l.code
+      });
+    case "hi":
+      return hi.map(function (l) {
+        return l.code
+      });
+    case "ml":
+      return ml.map(function (l) {
+        return l.code
+      });
+    case "tl":
+      return tl.map(function (l) {
+        return l.code
+      });
+    case "ur":
+      return ur.map(function (l) {
+        return l.code
+      });
     default:
       return []
   }
 };
 
 LanguageList.prototype.getData = function getData(lang) {
-  switch(lang){
-    case "en": return en;
-    case "ar": return ar;
-    default: return [];
-  }};
+  switch (lang) {
+    case "en":
+      return en;
+    case "ar":
+      return ar;
+    case "hi":
+      return hi;
+    case "ml":
+      return ml;
+    case "tl":
+      return tl;
+    case "ur":
+      return ur;
+    default:
+      return [];
+  }
+};
--- test/get-data.js
+++ test/get-data.js
@@ -4,10 +4,18 @@ const test = require('tap').test;
 const pkg = require('../')();
 const en = require('../lang/en.json');
 const ar = require('../lang/ar.json');
+const hi = require("../lang/hi.json");
+const ml = require("../lang/ml.json");
+const tl = require("../lang/tl.json");
+const ur = require("../lang/ur.json");
 
 test('get all language names and code', function(t) {
   t.equal(pkg.getData("en").length, en.length, 'data list should be the same length as json');
   t.equal(pkg.getData("ar").length, ar.length, 'data list should be the same length as json');
+  t.equal(pkg.getData("hi").length, hi.length, 'data list should be the same length as json');
+  t.equal(pkg.getData("ml").length, ml.length, 'data list should be the same length as json');
+  t.equal(pkg.getData("tl").length, tl.length, 'data list should be the same length as json');
+  t.equal(pkg.getData("ur").length, ur.length, 'data list should be the same length as json');
   t.equal(pkg.getData("xy").length, 0, 'data list should be 0');
   t.type(pkg.getData("en"), Array, 'data list is an array');
   t.type(pkg.getData("en")[0], Object, 'first item in list should be object');
